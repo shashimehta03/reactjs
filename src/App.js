1527edb06127217c45aa3a04675d0d48a208b564
shashimehta03 <itsmeshashi29@gmail.com>
2024-07-14 23:55:16 +0530

    routing feature

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 //import Greeting from './greeting'; //props
 import Counter from './Counter'; //useState
 
+import Home from './Pages/home';
+import About from './Pages/about';
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 const App = () => {
   return (
     // <div>
@@ -10,10 +14,34 @@ const App = () => {
     //   <Greeting name="Alice" />
     //   {/* //passing value in pros for greeting components  */}
     // </div>
-    <>
-      <Counter/> 
-      {/* use state implemnteed here */}
-    </>
+    // <>
+    //   <Counter/> 
+    //   {/* use state implemnteed here */}
+    // </>
+    <Router>
+      <div>
+       <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>about</Link>
+          </li>
+          <li>
+            <Link to='/counter'>Counter</Link>
+          </li>
+        </ul>
+       </nav>
+       <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>} />
+        <Route path='/counter' element={<Counter/>}/>
+
+       </Routes>
+      </div>
+    </Router>
+
     
   );
 };
